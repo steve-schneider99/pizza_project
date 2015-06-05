6327a64693ac85e804f0aedad333741646cdd9f7
steve-schneider99 <steve.schneider99@gmail.com>
2015-06-05 15:48:19 -0700

    fixed size choice bug

--- js/scripts.js
+++ js/scripts.js
@@ -24,10 +24,9 @@ Pizza.prototype.pizzaSizeCost = function () {
 };
 
 Pizza.prototype.pizzaCost = function () {
-  var cost = (((this.pizzaSizeCost()) + ((this.toppings.length) * .5)) * (this.quantity));
+  var cost = (((this.pizzaSizeCost) + ((this.toppings.length) * .5)) * (this.quantity));
   return cost;
 };
-
 
 $(document).ready(function() {
   var pizza = new Pizza(0, [], "");
@@ -35,30 +34,48 @@ $(document).ready(function() {
     $("#start-pizza").slideUp();
     $(".pizzaSize-form").delay(750).fadeIn(750);
     $("#great-choice").hide();
+      $("#debug-size").text(pizza.pizzaSize);
+      $("#debug-toppings").text(pizza.toppings);
+      $("#debug-quantity").text(pizza.quantity);
+      $("#debug-cost").text(pizza.pizzaCost);
   });
 
   $("#size-choice").change(function() {
-    var pizzaSize = parseInt($("option#size-choice").val());
+    var pizzaSize = $("#size-choice option:selected").val();
     pizza.changeSize(pizzaSize);
+
     // $("#great-choice").show();
     // $("#great-choice").delay(750).hide();
     $(".pizzaSize-form").slideUp();
     $(".toppings-form").delay(750).fadeIn(750);
+      $("#debug-size").text(pizza.pizzaSize);
+      $("#debug-toppings").text(pizza.toppings);
+      $("#debug-quantity").text(pizza.quantity);
+      $("#debug-cost").text(pizza.pizzaCost);
   });
 
   $("#toppings-button").click(function() {
     var pizzaToppings = [];
     $(".toppings-form").slideUp();
     $(".quantity-form").delay(750).fadeIn(750);
+      $("#debug-size").text(pizza.pizzaSize);
+      $("#debug-toppings").text(pizza.toppings);
+      $("#debug-quantity").text(pizza.quantity);
+      $("#debug-cost").text(pizza.pizzaCost);
   });
 
   $("#quantity-submit").click(function() {
-    var pizzaQuantity = 0;
+    var pizzaQuantity = parseInt($("input#inputQuantity").val());
+    pizza.changeQuantity(pizzaQuantity);
 
     $(".quantity-form").slideUp();
     $(".pizza-cost").delay(750).fadeIn(750);
+      $("#debug-size").text(pizza.pizzaSize);
+      $("#debug-toppings").text(pizza.toppings);
+      $("#debug-quantity").text(pizza.quantity);
+      $("#debug-cost").text(pizza.pizzaCost);
+    });
 
-  });
 
 
 });
